d9d98253ed23aba0606282212dfa2d0bcc500f07
kavya <vkavyanjali997@gmail.com>
2026-03-05 10:28:08 +0530

    mind care

--- app/src/main/java/com/simats/nutrisoul/BedtimeReminderUtils.kt
+++
@@ -1,4 +0,0 @@
-package com.simats.nutrisoul
-
-// This file is deprecated. All alarm functions have been moved to MindCareAlarms.kt
-// to avoid conflicting overloads and to centralize alarm management.
